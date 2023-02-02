035782cd1630715e8281f92619416103678a0c6a
Vlry777 <camiferulloa@gmail.com>
2023-02-02 20:50:47 -0300

    edit profile,!! error in image!!

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,12 @@
 from django.contrib.auth.views import LogoutView
 from django.urls import path
 
-from users.views import login_view, register
+from users.views import login_view, register, update_user
 
 urlpatterns= [
     path('login/', login_view, name= 'login'),
     path('logout/', LogoutView.as_view(template_name = 'users/logout.html')),
     path('singup/', register),
+    path('update/', update_user),
 
 ]
