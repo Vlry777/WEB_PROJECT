e4a23d8e2868567985c25530c38821719e3e4ea3
Vlry777 <camiferulloa@gmail.com>
2023-02-08 22:44:47 -0300

    Updated reedme

--- academy/views.py
+++ academy/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'index/index.html', context={})
+    return render(request, 'index.html', context={})
 
 def about(request):
     return render(request,'about/about.html', context={})
