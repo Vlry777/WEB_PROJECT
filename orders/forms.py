cedb8df10d93a231c08bd5336db023095b3916a2
Vlry777 <camiferulloa@gmail.com>
2023-02-08 23:27:05 -0300

    final updates

--- orders/forms.py
+++ orders/forms.py
@@ -1,7 +1,8 @@
 from django import forms
+from .models import Order
 
 class OrdersForm(forms.Form):
-    client = forms.CharField(max_length=50)
-    product = forms.CharField(max_length=100)
-    creation_time= forms.DateTimeField()
-    payment_method = forms.CharField()
+    client = forms.CharField(label='Cliente',max_length=50)
+    product = forms.CharField(label= 'Producto',max_length=100)
+    creation_time= forms.DateTimeField(label='Fecha de creacion')
+    payment_method = forms.ChoiceField(label= 'Metodo de pago', choices=Order.CHOICES)
